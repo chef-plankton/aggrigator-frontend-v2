4b1eedce7f1528fb8e39fa6eb3223f0d2f9849bc
msghadiri <ghadirimohammadsaeed@gmail.com>
2022-06-25 14:46:54 +0430

    redux route slippage

--- src/features/route/routeSlice.tsx
+++ src/features/route/routeSlice.tsx
@@ -4,12 +4,14 @@ interface RouteState {
   fromToken: number;
   toChain: number;
   toToken: number;
+  slippageTolerance: number;
 }
 const initialState: RouteState = {
   fromChain: 56,
   fromToken: 0,
   toChain: 56,
   toToken: 0,
+  slippageTolerance: 0.01,
 };
 export const chainsSlice = createSlice({
   name: "route",
@@ -27,6 +29,9 @@ export const chainsSlice = createSlice({
     changeToToken: (state, action) => {
       state.toToken = action.payload;
     },
+    changeSlippageTolerance: (state, action) => {
+      state.slippageTolerance = action.payload;
+    },
   },
 });
 
@@ -36,6 +41,7 @@ export const {
   changeFromToken,
   changeToChain,
   changeToToken,
+  changeSlippageTolerance,
 } = chainsSlice.actions;
 
 export default chainsSlice.reducer;
