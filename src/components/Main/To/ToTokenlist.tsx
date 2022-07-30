1677647f3377830264ed096770aa6ef41d008a89
Morteza Akbari <morteza.akbari94@gmail.com>
2022-07-30 23:27:24 +0430

    token list

--- src/components/Main/To/ToTokenlist.tsx
+++ src/components/Main/To/ToTokenlist.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -28,17 +28,94 @@ const StyledInput = styled.input<HTMLAttributes<HTMLInputElement>>`
 function ToTokenlist() {
   const dispatch = useDispatch();
   const chainId = useSelector(({ route }: RootState) => route.toChain);
-  const { isLoading, data, refetch } = useQuery("tokens", () => {
+  const [tokens, setTokens] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [orgListItems, setOrgListItems] = useState([]);
+  const [listItems, setListItems] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [page, setPage] = useState(0);
+
+  // const { isLoading, data } = useQuery("tokens", () => {
+  //   if (chainId === 56) {
+  //     return axios.get("http://192.64.112.22:8084/tokens?chain=bsc");
+  //   }
+  //   if (chainId === 250) {
+  //     return axios.get("http://192.64.112.22:8084/tokens?chain=fantom");
+  //   }
+  //   if (chainId === 97) {
+  //     return axios.get("http://localhost:4000/BSC-testnet");
+  //   }
+  // });
+
+  useEffect(() => {
+    // Initial setup
+    let url = '';
+
     if (chainId === 56) {
-      return axios.get("http://192.64.112.22:8084/tokens?chain=bsc");
+      url = "http://192.64.112.22:8084/tokens?chain=bsc&limit=6000";
     }
     if (chainId === 250) {
-      return axios.get("http://192.64.112.22:8084/tokens?chain=fantom");
+      url = "http://192.64.112.22:8084/tokens?chain=fantom&limit=6000";
     }
     if (chainId === 97) {
-      return axios.get("http://localhost:4000/BSC-testnet");
+      url = "http://localhost:4000/BSC-testnet";
     }
-  });
+
+    axios.get(url)
+    .then((res) => {
+      setOrgListItems(res.data); 
+      setTokens(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [tokens]);
+
+  const handleScroll = (e) => {
+    let myDiv = e.target;
+    if (
+      myDiv.offsetHeight + myDiv.scrollTop < myDiv.scrollHeight ||
+      isFetching
+    )
+      return;
+    setIsFetching(true);
+  };
+
+  const fetchData = async () => {
+    if (orgListItems.length == 0) return;
+      const data1 = orgListItems.slice(page*10, (page+1)*10);
+      if (page == 0) {
+        setListItems(data1);
+      } else {
+        setListItems([...listItems, ...data1])
+      }
+      setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (!isFetching) return;
+        fetchMoreListItems();
+  }, [isFetching]);
+
+  const fetchMoreListItems = () => {
+    fetchData();
+    setIsFetching(false);
+  };
+
+  useEffect(() => {
+    if (searchQuery.slice(0, 2) == "0x") {
+      setOrgListItems(tokens.filter(c => c.contract_addr.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1));
+      setPage(0);
+      setIsFetching(true);
+    } else {
+      setOrgListItems(tokens.filter(c => c.symbol.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1));
+      setPage(0);
+      setIsFetching(true);
+    }
+  
+}, [searchQuery]);
+
   return (
     <>
       <div className="flex justify-between items-center mb-5 pt-5 pr-5 pl-5">
@@ -54,24 +131,31 @@ function ToTokenlist() {
           />
         </div>
       </div>
+
+      {/* Tokens list search box */}
       <div className="px-5">
-        <StyledInput type="text" placeholder="Search name or paste address" />
+        <StyledInput type="text" placeholder="Search name or paste address" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
       </div>
-      <div className="flex flex-col w-[100%] overflow-y-scroll h-[500px] scrollbar">
-        {isLoading ? (
+
+      {/* Tokens list box */}
+      <div className="flex flex-col w-[100%] overflow-y-scroll h-[500px] scrollbar" onScroll={handleScroll}>
+        {tokens.length == 0 ? (
           // is Loading Component
           <FromTokenlistIsLoading />
         ) : (
-          data.data.map((token, index) => (
+          listItems.map((token, index) => (
             <ToToken token={token} index={index} />
           ))
         )}
       </div>
-      <div className="w-[100%] border-[1px] rounded-xl px-[12px] py-[15px] bg-[#edeef2] mt-2 cursor-pointer">
+
+      {/* Manage Token list */}
+      <div className="w-[100%] border-[1px] rounded-xl rounded-t-none px-[12px] py-[15px] bg-[#edeef2] mt-2 cursor-pointer">
         <p className="text-[14px] text-center">Manage Tokenlist</p>
       </div>
     </>
   );
+
 }
 
 export default ToTokenlist;
