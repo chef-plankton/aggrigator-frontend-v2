30cfd865e28322499589c7eeef1785aa58b7ef8c
msghadiri <ghadirimohammadsaeed@gmail.com>
2022-08-02 15:44:59 +0430

    fix token api types

--- src/config/api/types.ts
+++ src/config/api/types.ts
@@ -1,6 +1,8 @@
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 export type RequestPath = string;
 export interface tokenListApi {
+  contract_addr: string;
+  decimals: number;
   name: string;
   symbol: string;
 }
